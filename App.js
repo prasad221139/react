fdba2ef41fc0effcae90e670b758640c65a49df3
Kilaparthi <kilaparthi-durga.prasad@capgemini.com>
2026-01-02 17:27:31 +0530

    Episode 1

---
+++ App.js
@@ -0,0 +1,23 @@
+
+const parent = React.createElement("div",{id : "parent"},
+   [
+     React.createElement("div",{id : "child1"},[
+        React.createElement("h1",{},"I am h1 tag-c1"),
+        React.createElement("h2",{},"I am h2 tag-c1")
+    ]),
+     React.createElement("div",{id : "child2"},[
+        React.createElement("h1",{},"I am h1 tag-c2"),
+        React.createElement("h2",{},"I am h2 tag-c2")
+    ])
+   ]
+)
+
+
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello, World! from React"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render([   parent, heading]);
